7f378628288cd808e93c34700bf1199526de893a
Oscar Megía López <pfgytk@gmail.com>
2020-03-09 13:43:54 +0100

    Quitado use LoginForm y ContactForm de siteController

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -7,8 +7,6 @@ use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\Response;
 use yii\filters\VerbFilter;
-use app\models\LoginForm;
-use app\models\ContactForm;
 use app\models\Mensajes;
 use yii\data\ActiveDataProvider;
 use app\models\Libros;
